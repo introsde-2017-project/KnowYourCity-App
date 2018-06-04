d7c8769bcf8829a8a3f115315f1d55bdfb027fb8
Danish Cheema <danishcheemapk@yahoo.com>
2018-06-04 18:27:37 +0200

    heroku dep

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+import express, { static } from "express";
+import { join } from "path";
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
