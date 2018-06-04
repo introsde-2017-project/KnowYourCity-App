db03cf22d3f10d9c127ae3639cc702aca0693b0c
Danish Cheema <danishcheemapk@yahoo.com>
2018-06-04 20:31:01 +0200

    heroku dep

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path');
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
 
 const app = express();
+
+app.listen(process.env.PORT || 8080);
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
