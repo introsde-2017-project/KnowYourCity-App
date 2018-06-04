a5124d38278bc7311cb14f6d0aa996a35e1f2582
Danish Cheema <danishcheemapk@yahoo.com>
2018-06-04 20:15:06 +0200

    heroku dep

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src/app'));
 
 app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'src', 'index.html');
+    const index = path.join(__dirname, 'src/app', 'app.component.html');
     res.sendFile(index);
   });
 
